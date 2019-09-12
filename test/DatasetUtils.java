18c61f7f06c8bc3b9a09bc25dfb61c14f53c6638
libuwei <yi0_chen@163.com>
2019-09-12 16:09:53 +0800

    新增test/file2.txt,deleted file1.txt,modifed

--- test/DatasetUtils.java
+++ test/DatasetUtils.java
@@ -12,18 +12,24 @@ import org.jfree.data.general.PieDataset;
  */
 public class DatasetUtils {
 	/**
+	 * 增加了部分项目代码
+	 * 
 	 * @return CategoryDataset
 	 */
 	public static CategoryDataset createDataset(String excelPath) {
 		Map<String, Map<String, Double>> read2dXlsxMM = ExcelPoiUtils.read2DXlsxMM(excelPath);
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset();// 创建数据集对象
 		for(Entry<String, Map<String, Double>> entry : read2dXlsxMM.entrySet()) {
-//			System.out.println(entry.getKey()+", "+entry.getValue());
 			String rowKey = entry.getKey();
 			Map<String, Double> subMap = entry.getValue();
 			for (Entry<String, Double> subEntry: subMap.entrySet()) {
 				String columnKey = subEntry.getKey();
 				double value  = subEntry.getValue();
+				println();
+				println();
+				println();
+				println();
+				println();
 				dataset.addValue(value, rowKey, columnKey);
 			}
 		}
@@ -37,9 +43,7 @@ public class DatasetUtils {
 		DefaultPieDataset dataSet = new DefaultPieDataset();
 		Map<String, Double> read2dXlsxPie = ExcelPoiUtils.read2DXlsxPie(excelPath);
 		DefaultCategoryDataset dataset = new DefaultCategoryDataset();// 创建数据集对象
-		for(Entry<String, Double> entry : read2dXlsxPie.entrySet()) {
-			dataSet.setValue(entry.getKey(),entry.getValue());
-		}
+		-------------
 		return dataSet;
 	}
 	
